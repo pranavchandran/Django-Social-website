84de03f85b13b90e4293b46af6b82ef57451bf0a
pranavchandran <pranav.chandran@gmail.com>
2021-03-31 09:14:38 +0530

    Less than 1 min stopped identical Actions

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'actions.apps.ActionsConfig',
     'easy_thumbnails',
     'images.apps.ImagesConfig',
     'django_extensions',
